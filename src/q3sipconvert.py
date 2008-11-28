ab85ec78d35cc98a2b89a830a9fe7ed2a02f121a
Matteo Bertini <matteo@naufraghi.net>
2008-11-28 18:33:54 +0100

    Fix the misleading comment (and rollback the code fix)

--- src/q3sipconvert.py
+++ src/q3sipconvert.py
@@ -516,10 +516,10 @@ def process(filename, line, c, comment_lines={}):
         if "class QMenuData" in line: # we need only QMenuItem
             comment_lines[filename] = True
     elif "q3mime.sip" in filename:
-        if "class QMimeSource" in line: # we need only QMimeSource
-            comment_lines[filename] = False
-        elif "class Q3MimeSourceFactory" in line:
+        if "class QMimeSource" in line: # we exlude only QMimeSource
             comment_lines[filename] = True
+        elif "class Q3MimeSourceFactory" in line:
+            comment_lines[filename] = False
     if comment_lines.get(filename, False):
         line = "// "+line
     if oldline != line and "-----" in filename:
