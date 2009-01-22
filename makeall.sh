c0a922b35ab60e0a8af1bbf243cdc191d302ba37
Matteo Bertini <matteo@naufraghi.net>
2009-01-22 23:58:18 +0100

    Version bump

--- makeall.sh
+++ makeall.sh
@@ -4,6 +4,6 @@ cd sip-4.7.9
 python configure.py -b ../build -d ../build -e ../build -v ../build
 make && make install
 cd ..
-cd PyQt3Support-PyQt4.4.4-gpl-r4
+cd PyQt3Support-PyQt4.4.4-gpl-r5-pre
 PYTHONPATH=../sip-4.7.9 python configure.py -c -j3 -g --confirm-license -b ../build -d ../build --no-sip-files -p ../build --no-qsci-api -e QtGui -e Qt3Support
 make && make install
