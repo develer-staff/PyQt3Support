a29e4bcbcec5df04e5680576c612cc00bb25a5ee
Matteo Bertini <matteo@naufraghi.net>
2009-01-22 23:23:21 +0100

    Add rude make all script

---
+++ makeall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir build
+cd sip-4.7.9
+python configure.py -b ../build -d ../build -e ../build -v ../build
+make && make install
+cd ..
+cd PyQt3Support-PyQt4.4.4-gpl-r4
+PYTHONPATH=../sip-4.7.9 python configure.py -c -j3 -g --confirm-license -b ../build -d ../build --no-sip-files -p ../build --no-qsci-api -e QtGui -e Qt3Support
+make && make install
